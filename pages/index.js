f3e1358d5a6df9b688e8d2b5667f8287d187809e
Arsenic333 <stanbond17@gmail.com>
2022-01-28 12:35:11 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>eragerageagerher!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Hey Dowit!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          arib fat lol</code>
         </p>
       </main>
 
